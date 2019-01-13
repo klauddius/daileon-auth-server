858eaea2b05d758e2bd6a61febe79a0fe4d418a9
Claudio Brito Moreno <klauddius@gmail.com>
2019-01-13 14:38:04 -0200

    Initial commit

---
+++ src/main/java/br/com/klauddius/daileon/oauth/DaileonAuthServerApplication.java
@@ -0,0 +1,14 @@
+package br.com.klauddius.daileon.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaileonAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaileonAuthServerApplication.class, args);
+	}
+
+}
+
